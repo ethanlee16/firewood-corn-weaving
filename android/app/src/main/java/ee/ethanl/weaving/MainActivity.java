67ae52c56fbd3971d2ead698c68cbf8894ac38cd
Ethan Lee <hello@ethanl.ee>
2021-02-24 23:10:13 -0800

    Add capacitor

---
+++ android/app/src/main/java/ee/ethanl/weaving/MainActivity.java
@@ -0,0 +1,23 @@
+package ee.ethanl.weaving;
+
+import android.os.Bundle;
+
+import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Plugin;
+
+import java.util.ArrayList;
+
+import com.ahm.capacitor.camera.preview.CameraPreview;
+
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    // Initializes the Bridge
+    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
+      // Additional plugins you've installed go here
+        add(CameraPreview.class);
+    }});
+  }
+}
